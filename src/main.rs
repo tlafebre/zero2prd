fc33e705501b20de9b69af222955b697145eb26e
Tjeerd Lafebre <tjeerd.lafebre@ing.com>
2021-09-21 15:48:07 +0200

    server prints on what port it is running

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use zero2prd::run;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
 
+    println!("Server running on port: {}", port);
     run(listener)?.await
 }
